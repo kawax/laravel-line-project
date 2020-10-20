264070e568deea78776ff5bb6d1c9e0cc4ca7ee7
kawax <kawaxbiz@gmail.com>
2020-10-20 09:40:32 +0900

    wip

--- config/app.php
+++ config/app.php
@@ -227,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Bot' => Revolution\Line\Facades\Bot::class,
     ],
 
 ];
