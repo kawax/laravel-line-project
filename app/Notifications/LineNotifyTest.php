56149fe4fb380aec8969f0b25fa6808ce3ec4772
kawax <kawaxbiz@gmail.com>
2023-04-13 14:25:36 +0900

    wip

--- app/Notifications/LineNotifyTest.php
+++ app/Notifications/LineNotifyTest.php
@@ -13,39 +13,27 @@ class LineNotifyTest extends Notification
     use Queueable;
 
     /**
-     * @var string
-     */
-    protected $message;
-
-    /**
      * Create a new notification instance.
      *
      * @param  string  $message
      * @return void
      */
-    public function __construct($message)
+    public function __construct(protected string $message)
     {
-        $this->message = $message;
+        //
     }
 
     /**
      * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
      */
-    public function via($notifiable)
+    public function via(object $notifiable): array
     {
         return [
             LineNotifyChannel::class,
         ];
     }
 
-    /**
-     * @param  mixed  $notifiable
-     * @return LineNotifyMessage
-     */
-    public function toLineNotify($notifiable)
+    public function toLineNotify(object $notifiable): LineNotifyMessage
     {
         return LineNotifyMessage::create($this->message)
             ->withSticker(1, random_int(1, 17));
