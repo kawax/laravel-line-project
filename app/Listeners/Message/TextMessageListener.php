d34b7b80055d047dbfacd67755c4cb42e8da3f0d
kawax <kawaxbiz@gmail.com>
2023-04-13 14:26:34 +0900

    wip

--- app/Listeners/Message/TextMessageListener.php
+++ app/Listeners/Message/TextMessageListener.php
@@ -5,10 +5,8 @@ namespace App\Listeners\Message;
 use App\Notifications\LineNotifyTest;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Notification;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
-use OpenAI\Laravel\Facades\OpenAI;
 use Revolution\Line\Messaging\Bot;
 
 class TextMessageListener
@@ -34,22 +32,12 @@ class TextMessageListener
         $token = $event->getReplyToken();
         $text = $event->getText();
 
-        $chat = OpenAI::chat()->create([
-            'model' => 'gpt-3.5-turbo',
-            'temperature' => 0.5,
-            'messages' => [
-                ['role' => 'user', 'content' => $text],
-            ],
-        ]);
-
-        $content = Arr::get($chat, 'choices.0.message.content');
-
         $response = Bot::reply($token)
             ->withSender(config('app.name'))
-            ->text($content);
+            ->text($text);
 
-//        Notification::route('line-notify', config('line.notify.personal_access_token'))
-//            ->notify(new LineNotifyTest($text));
+        Notification::route('line-notify', config('line.notify.personal_access_token'))
+            ->notify(new LineNotifyTest($text));
 
         if (! $response->isSucceeded()) {
             logger()->error(static::class.$response->getHTTPStatus(), $response->getJSONDecodedBody());
